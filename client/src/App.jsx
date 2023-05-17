710213afdbb7c81ac1f455dc15b49868ec6a24d0
yuwei wang <w67238@outlook.com>
2023-05-17 15:09:00 +1200

    frontend user settings icons setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,11 +30,11 @@ function App() {
           <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
             <div>Anywhere</div>
             {/* search bar separator */}
-            <div className='border-l border-gray-300'></div>
+            <div className="border-l border-gray-300"></div>
             <div>Any week</div>
-            <div className='border-l border-gray-300'></div>
+            <div className="border-l border-gray-300"></div>
             <div>Add guests</div>
-            <button className='bg-primary text-white p-1 rounded-full'>
+            <button className="bg-primary text-white p-1 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -52,8 +52,39 @@ function App() {
             </button>
           </div>
 
-        </header>
+          {/* user settings icon */}
+          <div className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
 
+            <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6 relative top-1"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+        </header>
       </div>
     </>
   )
