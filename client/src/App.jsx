4406938bbc091e4d56f3af36c48cdd00424d8879
yuwei wang <w67238@outlook.com>
2023-05-17 14:22:53 +1200

    frontend search bar setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,56 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <div>
         {/* icon area */}
-        <header className='p-4'>
+        <header className="p-4 flex justify-between">
           <a href="" className="flex items-center gap-1">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 -rotate-45">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13" />
-          </svg>
-          <span className='font-bold text-xl'>bnb</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-8 h-8 -rotate-45"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
+              />
+            </svg>
+            <span className="font-bold text-xl">bnb</span>
           </a>
-        </header>
 
-        {/* search bar */}
-        <div>
-          
-        </div>
+          {/* search bar */}
+          <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
+            <div>Anywhere</div>
+            {/* search bar separator */}
+            <div className='border-l border-gray-300'></div>
+            <div>Any week</div>
+            <div className='border-l border-gray-300'></div>
+            <div>Add guests</div>
+            <button className='bg-primary text-white p-1 rounded-full'>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </button>
+          </div>
+
+        </header>
 
       </div>
     </>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#E57373",
+      }
+    },
   },
   plugins: [],
 }
