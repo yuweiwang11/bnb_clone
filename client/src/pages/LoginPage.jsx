65ca0f92498cb08b2a53fcbb860d72e5b487848b
yuwei wang <w67238@outlook.com>
2023-05-17 19:42:05 +1200

    login page setup

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,14 @@
 function LoginPage(){
   return (
     <>
-    <div>
-
+    <div className="mt-4">
+      <h1>Login</h1>
+      <form>
+        <input type="email" placeholder={"your@email.com"}/>
+        <input type="passwd" placeholder="password" />
+        <button>Login</button>
+        <button>Sign up</button>
+      </form>
     </div>
     
     </>
