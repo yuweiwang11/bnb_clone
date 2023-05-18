ec95cc412d44665a922a3bf3f4dd0434a57dac9a
yuwei wang <w67238@outlook.com>
2023-05-18 18:36:52 +1200

    cors setup send request to api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,22 +1,46 @@
-import {Link} from "react-router-dom"
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from "axios"
 
-function SignUpPage(){
+function SignUpPage() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function userSignUp(e){
+    // send request to api
+    e.preventDefault()
+    axios.get("http://localhost:4000/test")
+  }
+
   return (
     <>
-    <div className="mt-4 grow flex items-center justify-around">
-      <div className="-mt-48">
-        <h1 className="text-4xl text-center mb-4">Sign Up</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="Your full name"/>
-          <input type="email" placeholder="Your@email.com"/>
-          <input type="passwd" placeholder="Password" />
-          <button className="primary">Sign Up</button>
-          <div className="text-right py-2 text-gray-700">
-            <p>Already a member? <Link className="underline text" to={"/login"} >Log in</Link></p>
-          </div>
-        </form>
+      <div className="mt-4 grow flex items-center justify-around">
+        <div className="-mt-48">
+          <h1 className="text-4xl text-center mb-4">Sign Up</h1>
+          <form onSubmit={userSignUp} className="max-w-md mx-auto">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Your full name"
+            />
+            <input type="email" value={email}
+              onChange={(e) => setEmail(e.target.value)} placeholder="Your@email.com" />
+            <input type="passwd" value={password}
+              onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button className="primary">Sign Up</button>
+            <div className="text-right py-2 text-gray-700">
+              <p>
+                Already a member?{' '}
+                <Link className="underline text" to={'/login'}>
+                  Log in
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   )
 }
