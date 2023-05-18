c40eb2ecd3a5189b20fe321ecc1cb6f5ad7deb0a
yuwei wang <w67238@outlook.com>
2023-05-18 14:30:48 +1200

    adjust login page

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -4,7 +4,7 @@ import Header from "./Header"
 
 function Layout() {
   return(
-    <div className="p-4">
+    <div className="p-4 flex flex-col min-h-screen">
       <Header />
       <Outlet />
     </div>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,17 @@
 function LoginPage(){
   return (
     <>
-    <div className="mt-4">
-      <h1>Login</h1>
-      <form>
-        <input type="email" placeholder={"your@email.com"}/>
-        <input type="passwd" placeholder="password" />
-        <button>Login</button>
-        <button>Sign up</button>
-      </form>
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="-mt-48">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="your@email.com"/>
+          <input type="passwd" placeholder="password" />
+          <button className="primary">Login</button>
+          <button >Sign up</button>
+        </form>
+      </div>
+      
     </div>
     
     </>
