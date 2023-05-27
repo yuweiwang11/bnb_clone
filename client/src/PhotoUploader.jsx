00c026669484ccce45dead95299333c155191968
Yuwei Wang <w67238@outlook.com>
2023-05-27 21:20:52 +1200

    code refactorring separate photoUploader

---
+++ client/src/PhotoUploader.jsx
@@ -0,0 +1,93 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+function PhotoUploader({ addedPhotots, onChange }) {
+  const [photoLink, setPhotoLink] = useState('')
+
+  async function addPhotoByLink(e) {
+    e.preventDefault()
+    const { data: filename } = await axios.post('/upload-by-link', {
+      link: photoLink,
+    })
+    onChange((prev) => {
+      return [...prev, filename]
+    })
+    setPhotoLink('')
+  }
+
+  function uploadPhoto(e) {
+    // target: the input
+    const files = e.target.files
+    const data = new FormData()
+    for (let i = 0; i < files.length; i++) {
+      data.append('photos', files[i])
+    }
+    axios
+      .post('/upload', data, {
+        headers: { 'Content-type': 'multipart/form-data' },
+      })
+      .then((response) => {
+        const { data: filenames } = response
+        onChange((prev) => {
+          return [...prev, ...filenames]
+        })
+      })
+  }
+  return (
+    <>
+      <div className="flex gap-2">
+        <input
+          value={photoLink}
+          onChange={(e) => {
+            setPhotoLink(e.target.value)
+          }}
+          type="text"
+          placeholder={'Add photos using links'}
+        />
+        <button
+          onClick={addPhotoByLink}
+          className="bg-gray-200 text-sm mt-2 mb-2 px-4 rounded-2xl"
+        >
+          Add Picture
+        </button>
+      </div>
+
+      <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-6 lg:grid-cols-6">
+        {addedPhotots.length > 0 &&
+          addedPhotots.map((link) => (
+            <div className="h-32 flex" key={addedPhotots.indexOf(link)}>
+              <img
+                className="rounded-2xl w-full object-cover"
+                src={'http://localhost:4000/uploads/' + link}
+                alt="link"
+              />
+            </div>
+          ))}
+        <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+          <input
+            type="file"
+            multiple
+            className="hidden"
+            onChange={uploadPhoto}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+        </label>
+      </div>
+    </>
+  )
+}
+export default PhotoUploader
--- client/src/pages/ListingsPage.jsx
+++ client/src/pages/ListingsPage.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from 'react-router-dom'
 import { useState } from 'react'
 import Amenities from '../Amenities'
 import axios from 'axios'
+import PhotoUploader from '../PhotoUploader'
 
 function ListingsPage() {
   const { action } = useParams()
@@ -9,7 +10,7 @@ function ListingsPage() {
   const [title, setTitle] = useState('')
   const [address, setaddress] = useState('')
   const [addedPhotots, setAddedPhotots] = useState([])
-  const [photoLink, setPhotoLink] = useState('')
+
   const [description, setDescription] = useState('')
   const [amenities, setAmenities] = useState([])
   const [extraInfo, setExtraInfo] = useState('')
@@ -32,36 +33,6 @@ function ListingsPage() {
         {inputDescription(description)}
       </>
     )
-  }
-
-  async function addPhotoByLink(e) {
-    e.preventDefault()
-    const { data: filename } = await axios.post('/upload-by-link', {
-      link: photoLink,
-    })
-    setAddedPhotots((prev) => {
-      return [...prev, filename]
-    })
-    setPhotoLink('')
-  }
-
-  function uploadPhoto(e) {
-    // target: the input
-    const files = e.target.files
-    const data = new FormData()
-    for (let i = 0; i < files.length; i++) {
-      data.append('photos', files[i])
-    }
-    axios
-      .post('/upload', data, {
-        headers: { 'Content-type': 'multipart/form-data' },
-      })
-      .then((response) => {
-        const { data: filenames } = response
-        setAddedPhotots((prev) => {
-          return [...prev, ...filenames]
-        })
-      })
   }
 
   return (
@@ -173,58 +144,10 @@ function ListingsPage() {
             />
 
             {preInput('Photos', '')}
-            <div className="flex gap-2">
-              <input
-                value={photoLink}
-                onChange={(e) => {
-                  setPhotoLink(e.target.value)
-                }}
-                type="text"
-                placeholder={'Add photos using links'}
-              />
-              <button
-                onClick={addPhotoByLink}
-                className="bg-gray-200 text-sm mt-2 mb-2 px-4 rounded-2xl"
-              >
-                Add Picture
-              </button>
-            </div>
-
-            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-6 lg:grid-cols-6">
-              {addedPhotots.length > 0 &&
-                addedPhotots.map((link) => (
-                  <div className="h-32 flex" key={addedPhotots.indexOf(link)}>
-                    <img
-                      className="rounded-2xl w-full object-cover"
-                      src={'http://localhost:4000/uploads/' + link}
-                      alt="link"
-                    />
-                  </div>
-                ))}
-              <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-                <input
-                  type="file"
-                  multiple
-                  className="hidden"
-                  onChange={uploadPhoto}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                  />
-                </svg>
-                Upload
-              </label>
-            </div>
+            <PhotoUploader
+              addedPhotots={addedPhotots}
+              onChange={setAddedPhotots}
+            />
 
             <button className="primary my-4">Save</button>
           </form>
