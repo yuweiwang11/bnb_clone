b2890bd2e22042cd3b6f13ae831e928f02049bbb
Yuwei Wang <w67238@outlook.com>
2023-06-07 20:51:07 +1200

    finish bookings page

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -2,6 +2,8 @@ import AccountNav from '../AccountNav'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import PlaceImg from '../PlaceImg'
+import { differenceInCalendarDays, format } from 'date-fns'
+import { Link } from 'react-router-dom'
 
 function BookingsPage() {
   const [bookings, setBookings] = useState([])
@@ -18,12 +20,29 @@ function BookingsPage() {
       <div>
         {bookings?.length > 0 &&
           bookings.map((booking) => (
-            <div className="flex gap-4 bg-gray-200" key={booking._id}>
+            <Link
+              to={`/account/bookings/${booking._id}`}
+              className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden"
+              key={booking._id}
+            >
               <div className="w-48">
-                <PlaceImg listing={bookings[0].place} />
+                <PlaceImg listing={booking.place} />
               </div>
-              {booking.checkIn}
-            </div>
+              <div className="py-3 pr-3 grow">
+                <h2 className="text-xl">{booking.place.title}</h2>
+                <div className="border-t gap-2 items-center border-gray-300 mt-2 py-2">
+                  {format(new Date(booking.checkIn), 'yyyy-MM-dd')} &rarr;{' '}
+                  {format(new Date(booking.checkOut), 'yyyy-MM-dd')}
+                </div>
+                <div className="text-xl flex gap-1">
+                  {differenceInCalendarDays(
+                    new Date(booking.checkOut),
+                    new Date(booking.checkIn)
+                  )}{' '}
+                  Night(s) | Price: ${booking.price}
+                </div>
+              </div>
+            </Link>
           ))}
       </div>
     </div>
